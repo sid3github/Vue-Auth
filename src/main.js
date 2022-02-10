b162accced61a881ee889012caa2c006801f5d7e
unknown <siddharthpadwal3@gmail.com>
2022-02-11 03:10:48 +0530

    created vue login page with authentication using firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
-createApp(App).use(router).mount('#app')
+const firebaseConfig = {
+  apiKey: "AIzaSyAFuVXxeDNsFG1zHjUlzPMO_MgT8fXIfD8",
+  authDomain: "vue-auth-1a742.firebaseapp.com",
+  projectId: "vue-auth-1a742",
+  storageBucket: "vue-auth-1a742.appspot.com",
+  messagingSenderId: "659820764738",
+  appId: "1:659820764738:web:fd931d12f8af4649aad7db",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+createApp(App).use(router).mount("#app");
